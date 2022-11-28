1af6f4e7b2d16a0bb4b909618d4642607b41c223
Eiman Asghar <65137832+EimanAsghar@users.noreply.github.com>
2022-11-28 23:06:01 +0300

    change

--- js/app.js
+++ js/app.js
@@ -51,27 +51,20 @@ function buildnav() {
 buildnav()
 
 // Add class 'active' to section when near top of viewport
-
-const navbarmenu = document.querySelectorAll('li')
-
 function activesection() {
-    let i = 0
     sections.forEach((section) => {
         const top = section.getBoundingClientRect().top
 
         // if the distance to the top is between 0-100px, set the class as active
         if (top >= 0 && top < 100) {
             section.classList.add('your-active-class');
-            navbarmenu[i].style.cssText = 'background: green; color: #fff;'
+
 
             // else, remove the active 
         } else {
             section.classList.remove('your-active-class');
-            navbarmenu[i].style.cssText = 'background: white; color: #fff;'
 
         }
-
-        i++
     }
     )
 }
@@ -107,4 +100,27 @@ window.addEventListener('scroll', function () {
 
 
 // ref https://dev.to/areeburrub/change-nav-link-s-style-as-you-scroll-4p62
+const navbarmenu = document.querySelectorAll('.menu__link')
+
+
+window.onscroll = () => {
+    var current = "";
+  
+    sections.forEach((section) => {
+        const top = section.getBoundingClientRect().top
+      if (top >= 0 && top < 100) {
+        current = section.getAttribute("id"); }
+    });
+  
+    navbarmenu.forEach((li) => {
+
+      li.classList.remove("active");
+
+      if (li.classList.contains(current)) {
+
+        li.classList.add("active");
+        
+      }
+    });
+  };
 
